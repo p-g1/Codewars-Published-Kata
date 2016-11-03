2fcc90c05cf9a50d34917ab4717e45941d96964b
Pete <petergarvin1@gmail.com>
2016-11-03 07:41:56 +0000

    tests

---
+++ Sort_and_Transform/Sort_and_Transform_tests.js
@@ -0,0 +1,19 @@
+Test.describe("Basic tests",_=>{
+Test.assertSimilar(sortTransform([121, 122, 123, 124, 125, 120, 122, 132]), 'yzz-xy}-}yx-xy}');
+Test.assertSimilar(sortTransform([111, 112, 113, 114, 115, 113, 114, 110]), 'oprn-nors-sron-nors');
+Test.assertSimilar(sortTransform([51, 62, 73, 84, 95, 100, 99, 126]), '3>c~-3>d~-~d>3-3>d~');
+Test.assertSimilar(sortTransform([66, 101, 55, 111, 113]), 'Beoq-7Boq-qoB7-7Boq');
+Test.assertSimilar(sortTransform([78, 117, 110, 99, 104, 117, 107, 115, 120, 121, 125]), 'Nuy}-Ncy}-}ycN-Ncy}');
+Test.assertSimilar(sortTransform([101, 48, 75, 105, 99, 107, 121, 122, 124]), 'e0z|-0Kz|-|zK0-0Kz|');
+Test.assertSimilar(sortTransform([80, 117, 115, 104, 65, 85, 112, 115, 66, 76, 62]), 'PuL>->Asu-usA>->Asu');
+Test.assertSimilar(sortTransform([91, 100, 111, 121, 51, 62, 81, 92, 63]), '[d\\?-3>oy-yo>3-3>oy');
+Test.assertSimilar(sortTransform([78, 93, 92, 98, 108, 119, 116, 100, 85, 80]), 'N]UP-NPtw-wtPN-NPtw');
+Test.assertSimilar(sortTransform([111, 121, 122, 124, 125, 126, 117, 118, 119, 121, 122, 73]), 'oyzI-Io}~-~}oI-Io}~');
+Test.assertSimilar(sortTransform([82, 98, 72, 71, 71, 72, 62, 67, 68, 115, 117, 112, 122, 121, 93]), 'Rby]->Cyz-zyC>->Cyz');
+Test.assertSimilar(sortTransform([99, 98, 97, 96, 81, 82, 82]), 'cbRR-QRbc-cbRQ-QRbc');
+Test.assertSimilar(sortTransform([66, 99, 88, 122, 123, 110]), 'Bc{n-BXz{-{zXB-BXz{');
+Test.assertSimilar(sortTransform([66, 87, 98, 59, 57, 50, 51, 52]), 'BW34-23Wb-bW32-23Wb');
+});
+
+Test.describe("Random Tests Coming Soon",_=>{
+});
