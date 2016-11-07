7e341970e729f4db9e01abbb1a9a5889243b728b
Pete <petergarvin1@gmail.com>
2016-11-07 19:10:25 +0000

    kata

---
+++ You_Only_Need_1_Beginner/You_Only_Need_1_Beginner.js
@@ -0,0 +1,11 @@
+// Kata: https://www.codewars.com/kata/57cc975ed542d3148f00015b
+
+/* Description:  You will be given an array (a) and a value (x). All you need
+to do is check whether the provided array contains the value.
+
+Array can contain numbers or strings. X can be either. Return true if
+the array contains the value, false if not. With strings you will need to
+account for case.*/
+
+// Solution:
+function check(a,x){return a.some( z => z == x)};
